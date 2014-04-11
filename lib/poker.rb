9fb3be8e53eb5220c08b8ced42c7e3db3438fcf3
Bryan Phillips <bdphilly@gmail.com>
2014-04-11 16:47:31 -0700

    recognize hands spec complete

--- lib/poker.rb
+++ lib/poker.rb
@@ -16,7 +16,6 @@ class Deck
 
   def initialize
     @all_cards = all_cards
-
   end
 
   def all_cards
@@ -33,4 +32,104 @@ class Deck
     self.all_cards.shuffle.pop(num)
   end
 end
+
+class Hand
+
+  attr_accessor :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def royal_flush?
+    suit = cards[0].suit
+
+    cards.each do |card|
+      return false unless [:ace, :king, :queen, :jack, :ten].include?(card.value)
+      return false if card.suit != suit
+    end
+
+    true
+  end
+
+  def four_of_kind?
+    occurences = Hash.new(0)
+
+    cards.each do |card|
+      occurences[card.value] += 1
+    end
+
+    occurences.has_value?(4)
+  end
+
+  def full_house?
+    three_of_kind? && two_of_kind?
+  end
+
+  def flush?
+    occurences = Hash.new(0)
+
+    cards.each do |card|
+      occurences[card.suit] += 1
+    end
+
+    occurences.has_value?(5)
+  end
+
+  def straight?
+    sort_hash = {}
+    Card::VALUES.each_with_index do |each, index|
+      sort_hash[each] = index
+    end
+
+    array_of_indicies = []
+
+    cards.each do |card|
+      array_of_indicies << sort_hash[card.value]
+    end
+
+    array_of_indicies.sort!
+
+    ((array_of_indicies.last - array_of_indicies.first) == 4) ||
+    ((array_of_indicies.last - array_of_indicies.first) == 12 ) &&
+    (array_of_indicies.uniq.count == 5)
+
+  end
+
+  def three_of_kind?
+    occurences = Hash.new(0)
+
+    cards.each do |card|
+      occurences[card.value] += 1
+    end
+
+    occurences.has_value?(3)
+  end
+
+  def two_pair?
+    occurences = Hash.new(0)
+    pairs = []
+
+    cards.each do |card|
+      occurences[card.value] += 1
+    end
+
+    occurences.each do |k, v|
+      pairs << k if v == 2
+    end
+
+    pairs.length == 2
+  end
+
+  def two_of_kind?
+    occurences = Hash.new(0)
+
+    cards.each do |card|
+      occurences[card.value] += 1
+    end
+
+    occurences.has_value?(2)
+  end
+end
+
 
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -46,7 +46,154 @@ describe Deck do
       expect(deck.deal(3).count).to_not eq(5)
     end
   end
-
-
-
 end
+
+describe Hand do
+
+  let(:deck) { Deck.new }
+  let(:hand) { Hand.new(deck.deal) }
+
+  describe '#initialize' do
+    it 'should contain five cards' do
+      expect(hand.cards.count).to eq(5)
+    end
+  end
+
+  describe 'recognizes hands' do
+
+      it "should recognize a royal flush" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:heart, :king),
+          Card.new(:heart, :queen),
+          Card.new(:heart, :jack),
+          Card.new(:heart, :ten)
+          ])
+        expect(hand.royal_flush?).to be true
+      end
+
+      it "should recognize four of a kind" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:heart, :ace),
+          Card.new(:spade, :ace),
+          Card.new(:heart, :ace),
+          Card.new(:heart, :ten)
+          ])
+        expect(hand.four_of_kind?).to be true
+      end
+
+      it "should recognize a full house" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:heart, :ace),
+          Card.new(:heart, :four),
+          Card.new(:heart, :four),
+          Card.new(:spade, :four)
+          ])
+        expect(hand.full_house?).to be true
+      end
+
+      it "should recognize a flush" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:heart, :ace),
+          Card.new(:heart, :four),
+          Card.new(:heart, :four),
+          Card.new(:heart, :four)
+          ])
+        expect(hand.flush?).to be true
+      end
+
+      it "should recognize a straight without an ace" do
+        hand = Hand.new([
+          Card.new(:heart, :eight),
+          Card.new(:heart, :seven),
+          Card.new(:heart, :six),
+          Card.new(:heart, :five),
+          Card.new(:spade, :four)
+          ])
+        expect(hand.straight?).to be true
+      end
+
+      it "should recognize a straight with a high ace" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:heart, :two),
+          Card.new(:heart, :three),
+          Card.new(:heart, :four),
+          Card.new(:spade, :five)
+          ])
+        expect(hand.straight?).to be true
+      end
+
+      it "should recognize a straight with a low ace" do
+        hand = Hand.new([
+          Card.new(:heart, :ten),
+          Card.new(:heart, :jack),
+          Card.new(:heart, :queen),
+          Card.new(:heart, :king),
+          Card.new(:spade, :ace)
+          ])
+        expect(hand.straight?).to be true
+      end
+
+      it "should recognize three of a kind" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:heart, :ace),
+          Card.new(:heart, :ace),
+          Card.new(:heart, :nine),
+          Card.new(:spade, :ten)
+          ])
+        expect(hand.three_of_kind?).to be true
+      end
+
+      it "should recognize two pair" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:spade, :ace),
+          Card.new(:heart, :seven),
+          Card.new(:heart, :seven),
+          Card.new(:spade, :ten)
+          ])
+        expect(hand.two_pair?).to be true
+      end
+
+
+      it "should recognize two of a kind" do
+        hand = Hand.new([
+          Card.new(:heart, :ace),
+          Card.new(:heart, :ace),
+          Card.new(:heart, :four),
+          Card.new(:heart, :six),
+          Card.new(:spade, :ten)
+          ])
+        expect(hand.two_of_kind?).to be true
+      end
+
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
